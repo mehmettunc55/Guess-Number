14ab8f3b97fd7927a1c90c0dde377cd07006c188
mehmettunc55 <mtunc551980@gmail.com>
2024-02-26 14:33:30 +0100

    button added

--- game.js
+++ game.js
@@ -1,32 +1,58 @@
 
+//?PC'ye 1-20 arasında bir sayı tutturduk
+let rastgeleSayi =  Math.ceil(Math.random()*20)
+console.log(rastgeleSayi)
 
-let rasgeleSayi = Math.ceil(Math.random()*20)
+let mesaj = document.querySelector(".msg");
+
+let skor = 10;
+//skoru index.html'den çekebilirdik. Ama çok kullnacağımız için bu daha tercih edilen bir yol.
+let enYuksekSkor = 0;
 
 
-let mesaj  = document.querySelector('.msg')
-let skor = 10
-let enYuksekSkor = 0
+document.querySelector(".check").addEventListener("click", () => {
+    const tahmin = document.querySelector(".guess").value
+    // console.log(tahmin)
+    // document.querySelector(".guess").value = "";
 
-document.querySelector('check').addEventListener('click', () => {
-    const tahmin = document.querySelector('.guess').value
 
-if (!tahmin) {
-    mesaj.textContent = 'Lütfen Bir Sayi Giriniz'
-} else if (tahmin === rasgeleSayi){
-    mesaj.textContent = 'Tebrikler Bildiniz...👌'
-    document.querySelector('body').style.backgroundColor = 'green'
-    document.querySelector('.number').textContent = rasgeleSayi
-} else {
+    //!Tahmin girmeden butona basıldıysa
+    if (!tahmin) {
+        mesaj.textContent = "Lütfen bir sayı giriniz..."
 
-if (skor>1) {
-    skor--;
-    document.querySelector('.score').textContent = skor
-    tahmin<rasgeleSayi ? mesaj.textContent = 'Artir' : 
-} else {
-    
-}
+    //!Tahminim Doğruysa    
+    } else if(tahmin == rastgeleSayi) {
+        
+        mesaj.textContent = "Tebrikler Bildiniz...👏"
+        document.querySelector("body").style.backgroundColor = "green"
+        document.querySelector(".number").textContent = rastgeleSayi;
 
-}
+        // top-score Kontrolü yap 
 
-})
+        if (skor > enYuksekSkor ) {
+            enYuksekSkor = skor
+            document.querySelector(".top-score").textContent = skor
+        }
+    //!Tahmin Yanlışsa    
+    }else{
+        //? Skor 1'den büyük olduğu sürece hakkım var
+
+        if (skor > 1) {
+            skor--;
+            document.querySelector(".score").textContent = skor
+
+            tahmin < rastgeleSayi 
+            ? mesaj.textContent = "Arttır👆" 
+            : mesaj.textContent = "Azalt👇"
+        } else {
+            //! GAME OVER
+            mesaj.textContent = "Oyunu Kaybettiniz🥺"
+            document.querySelector(".score").textContent = 0;
+            document.querySelector("body").style.backgroundColor = "red"
+
+        }
+        
+    }
+
+} )
 
